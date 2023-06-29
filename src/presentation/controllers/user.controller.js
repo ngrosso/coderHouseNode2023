452ddfc798aaa135ffe87ae2f69faa0e2719dbb9
ngrosso <nzgrosso@gmail.com>
2023-06-29 19:50:00 -0300

    Se agregan try catches y logs de error

--- src/presentation/controllers/user.controller.js
+++ src/presentation/controllers/user.controller.js
@@ -3,41 +3,66 @@ import UserManager from "../../domain/managers/user.manager.js";
 export const list = async (req, res) => {
   const{ limit, page } = req.query;
   const manager = new UserManager();
-  const users = await manager.paginate({ limit, page });
-
-  res.send({ success: true, data: users.docs, ...users, docs: undefined });
+  try{
+    const users = await manager.paginate({ limit, page });
+  
+    res.status(200).json({ success: true, data: users.docs, ...users, docs: undefined });
+  }catch(e){
+    console.log(e);
+    res.status(400).json({ success: false, message: e.message });
+  }
 };
 
 export const getOne = async (req, res) => {
   const { id } = req.params;
 
   const manager = new UserManager();
-  const user = await manager.getOne(id);
-
-  res.send({ success: true, message: 'User found.', data: user });
+  try{
+    const user = await manager.getOne(id);
+  
+    res.status(200).json({ success: true, message: 'User found.', data: user });
+  }catch(e){
+    console.log(e);
+    res.status(400).json({ success: false, message: e.message });
+  }
 };
 
 export const save = async (req, res) => {
   const manager = new UserManager();
-  const user = await manager.create(req.body);
-
-  res.send({ success: true, message: 'User created.', data: user })
+  try{
+    const user = await manager.create(req.body);
+  
+    res.status(201).json({ success: true, message: 'User created.', data: user })
+  }catch(e){
+    console.log(e);
+    res.status(400).json({ success: false, message: e.message })
+  }
 };
 
 export const update = async (req, res) => {
   const { id } = req.params;
 
   const manager = new UserManager();
-  const result = await manager.updateOne(id, req.body);
-
-  res.send({ success: true, message: 'User updated.', data: result })
+  try{
+    const result = await manager.updateOne(id, req.body);
+  
+    res.status(200).json({ success: true, message: 'User updated.', data: result })
+  }catch(e){
+    console.log(e);
+    res.status(400).json({ success: false, message: e.message })
+  }
 };
 
 export const deleteOne = async (req, res) => {
   const { id } = req.params;
 
   const manager = new UserManager();
-  await manager.deleteOne(id);
-
-  res.send({ success: true, message: 'User deleted.' })
+  try{
+    await manager.deleteOne(id);
+  
+    res.status(200).json({ success: true, message: 'User deleted.' })
+  }catch(e){
+    console.log(e);
+    res.status(400).json({ success: false, message: e.message })
+  }
 };
