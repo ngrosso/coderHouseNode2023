ab16abdd72a31d7e2202dbd9597e03f50fabd76d
ngrosso <nzgrosso@gmail.com>
2023-07-06 23:46:12 -0300

    Se identa mejor el archivo

--- src/data/factories/dbFactory.js
+++ src/data/factories/dbFactory.js
@@ -1,14 +1,11 @@
 import MongooseAdapter from "./mongooseAdapter.js";
 
-class DbFactory
-{
-    static create(dbType)
-    {
+class DbFactory {
+    static create(dbType) {
         const dbs = new Map();
         dbs.set('MongooseAdapter', MongooseAdapter);
 
-        if(!dbs.has(dbType))
-        {
+        if (!dbs.has(dbType)) {
             throw Error('DbAdapter not found');
         }
 
