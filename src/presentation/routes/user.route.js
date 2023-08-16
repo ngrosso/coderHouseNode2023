642bb20bc371eb8bfc09c6d024aac5d6ce6e8070
ngrosso <nzgrosso@gmail.com>
2023-08-16 16:07:50 -0300

    Se agregan rutas de premium, documents falta desarrollar

--- src/presentation/routes/user.route.js
+++ src/presentation/routes/user.route.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import auth from '../middlewares/auth.middleware.js';
 import admin from '../middlewares/admin.middleware.js';
-import { list, deleteOne, getOne, save, update } from '../../presentation/controllers/user.controller.js';
+import { list, deleteOne, getOne, save, update, switchPremiumStatus, addDocument } from '../../presentation/controllers/user.controller.js';
 
 const usersRouter = Router();
 
@@ -10,5 +10,7 @@ usersRouter.get('/:id', auth, admin, getOne);
 usersRouter.post('/', auth, admin, save);
 usersRouter.put('/:id', auth, admin, update);
 usersRouter.delete('/:id', auth, admin, deleteOne);
+usersRouter.post('/premium/:id',auth, switchPremiumStatus)
+usersRouter.post('/:id/documents',auth,admin,addDocument) //TODO: Seguir despues de premium
 
 export default usersRouter;
