59048c8d14d6eaf2f373c20d00513ddd3d214cf2
ngrosso <nzgrosso@gmail.com>
2023-09-03 06:35:27 -0300

    Se agrega addUser atraves de commander

---
+++ Makefile
@@ -0,0 +1,2 @@
+admin:
+	NODE_ENV=dev node src/command.js addUser -e nzgrosso@gmail.com -fn Nicolas -ln Grosso -p 123456 -a 29 -ia true
--- src/presentation/commands/addUser.js
+++ src/presentation/commands/addUser.js
@@ -1,1 +1,31 @@
-//TODO: Implementar
+import { Command } from 'commander';
+import UserManager from '../../domain/managers/user.manager.js';
+
+const AddUserCommand = new Command('addUser');
+
+AddUserCommand
+  .version('0.0.1')
+  .description('Add user')
+  .option('-e, --email <email>', 'User`s email')
+  .option('-fn, --firstName <firstName>', 'User`s first name')
+  .option('-ln, --lastName <lastName>', 'User`s last name')
+  .option('-p, --password <password>', 'User`s password')
+  .option('-a, --age <age>', 'User`s age')
+  .option('-ia, --admin <admin>', 'User`s is admin')
+  .action(async (env) => {
+    const payload = {
+      ...env,
+      age: +env.age,
+      admin: env.admin === 'true',
+    };
+
+    console.log("payload", { ...payload, password: ">>>HIDDEN<<<" })
+    const manager = new UserManager();
+    const user = await manager.create(payload);
+
+    if (user) {
+      console.log('User created successfully');
+    }
+  });
+
+export default AddUserCommand;
